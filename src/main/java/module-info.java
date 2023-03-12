d0ce2d8e3d766359a4d25a9c841cc370c74ec338
Giovanni-ALLAGLO <alllaglo.j@gmail.com>
2023-03-12 19:35:28 +0100

    Ajoute les fichiers .jar et .properties de la librairie JPBC

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module com.example.projetcrypto {
     requires org.controlsfx.controls;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
+	requires jpbc.api;
+	requires jpbc.plaf;
 
     opens com.example.projetcrypto to javafx.fxml;
     exports com.example.projetcrypto;
