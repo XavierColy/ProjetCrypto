fb9682814351de744b74fd3bec3901f2a70bccee
Xavier Coly <xavierfcoly@gmail.com>
2023-03-19 06:18:38 +0100

    feat: add new message window

---
+++ src/main/java/com/example/projetcrypto/controllers/NewMessageController.java
@@ -0,0 +1,24 @@
+package com.example.projetcrypto.controllers;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * Creates a new window to send a message */
+public class NewMessageController {
+    //region attributes
+    public Button sendButton;
+    public Button cancelButton;
+    public Button SaveDraftButton;
+    public TextField recipientTextField;
+    public TextField bccTextField;
+    public TextField subjectField;
+    public Button joinAttachmentsButton;
+    public TextField attachmentsField;
+    public TextArea messageField;
+    //endregion
+
+    //region methods
+
+}
