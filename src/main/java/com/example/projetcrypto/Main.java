dc5ee81fd787983f3f0ad318b3ab54e502eb19a2
Xavier Coly <xavierfcoly@gmail.com>
2023-03-19 02:29:29 +0100

    feat: add login page to main project

---
+++ src/main/java/com/example/projetcrypto/Main.java
@@ -0,0 +1,35 @@
+package com.example.projetcrypto;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+//        // Load the FXML file for the main page
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainInterface.fxml"));
+//        Parent root = loader.load();
+//
+//        // Set the scene
+//        Scene scene = new Scene(root);
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+
+        // Load athe FXML file for the login page
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        Parent root = loader.load();
+
+        // Set the scene
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
