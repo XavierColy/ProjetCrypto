6c86dffab85ce1af62ab3d299ca4310a963cb844
Xavier Coly <xavierfcoly@gmail.com>
2023-03-19 15:31:45 +0100

    refactor: delete unnecessary code from TransitionController

--- src/main/java/com/example/projetcrypto/TransitionController.java
+++ src/main/java/com/example/projetcrypto/TransitionController.java
@@ -6,19 +6,22 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
+/**
+ * Permet de naviguer d'une fenêtre à une autre*/
 public class TransitionController {
 
+    /**Etape précédente*/
     private Stage prevStage;
 
     public void setPrevStage(Stage s) { this.prevStage = s; }
 
-    protected void transitionScene(String sceneTitle, String fxmlFileName, double width, double height) throws IOException {
+    protected void displayNextWindow(String fxmlFileName, boolean closePrev) throws IOException {
         Stage s = new Stage();
-        s.setTitle(sceneTitle);
-        Pane p = FXMLLoader.load(getClass().getResource("layedit.fxml"));
-        s.setScene(new Scene(p, width, height));
-        prevStage.close();
+        Pane p = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../fxml/"+fxmlFileName)));
+        s.setScene(new Scene(p));
+        if (closePrev) prevStage.close();
         s.show();
     }
 }
