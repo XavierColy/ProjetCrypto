c658fbae8f32ac68575bd5f46a0b5ba90cd1e51b
Xavier Coly <xavierfcoly@gmail.com>
2023-03-19 06:18:04 +0100

    feat: add main interface

---
+++ src/main/java/com/example/projetcrypto/controllers/MainViewController.java
@@ -0,0 +1,15 @@
+package com.example.projetcrypto.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MainViewController {
+    @FXML
+    public Button newMessageButton;
+    public Label receptionLabel;
+
+    public void displayNewMessageWindow(ActionEvent actionEvent) {
+    }
+}
