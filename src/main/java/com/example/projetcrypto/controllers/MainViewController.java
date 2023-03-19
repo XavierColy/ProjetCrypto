2747900a0700b0c5c4f9fd334d6950da9836ec98
Xavier Coly <xavierfcoly@gmail.com>
2023-03-19 15:34:33 +0100

    feat: add fxml attributes

--- src/main/java/com/example/projetcrypto/controllers/MainViewController.java
+++ src/main/java/com/example/projetcrypto/controllers/MainViewController.java
@@ -4,12 +4,29 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import javax.mail.Message;
 
 public class MainViewController {
     @FXML
     public Button newMessageButton;
-    public Label receptionLabel;
+    public Button answerButton;
+    public Button forwardButton;
+    public Label receivedMessagesLabel;
+    public Label sentMessagesLabel;
+    public Label draftMessagesLabel;
+    public Label spamMessagesLabel;
+    public Label trashLabel;
+    public ListView mailList;
 
-    public void displayNewMessageWindow(ActionEvent actionEvent) {
-    }
+
+//    @FXML
+//    private void init(){
+//        mailList= new ListView<Message>(Email.listMessageIDs("cryptotest10@outlook.com","Azerty@2023"));
+//    }
+//
+//    public void displayNewMessageWindow(ActionEvent actionEvent) {
+//        mailList= new ListView<Message>(Email.listMessageIDs("cryptotest10@outlook.com","Azerty@2023"));
+//    }
 }
