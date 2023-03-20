62e869fe2ec39c6e75e2327b9ee8e2e1d6ed0662
Xavier Coly <xavierfcoly@gmail.com>
2023-03-20 01:18:29 +0100

    feat: create emailModel

---
+++ src/main/java/com/example/projetcrypto/bo/EmailModel.java
@@ -0,0 +1,29 @@
+package com.example.projetcrypto.bo;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import java.io.IOException;
+
+public class EmailModel {
+    private final String id;
+    private final String sender;
+    private final String subject;
+    private final String text;
+
+    public EmailModel(Message message) throws MessagingException, IOException {
+        this.id = message.getHeader("Message-ID")[0];
+        this.subject = message.getSubject();
+        this.sender = message.getFrom()[0].toString();
+        this.text = message.getContent().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return this.sender + "\n" +
+                this.subject + " - " + this.text;
+    }
+}
